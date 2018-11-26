9e2c51ff5ad84504b78dc8aa988490974c5b46ee
wisemuji <wisemuji@gmail.com>
2018-11-27 00:01:12 +0900

    비번 변경

--- routes/auth/pw.js
+++ routes/auth/pw.js
@@ -2,13 +2,11 @@ var bcrypt = require('bcrypt');
 module.exports = (app, Users, rndstring)=>{
   app.post('/changepw', async(req,res)=>{
     console.log(req.body);
-    var new_pw;
     bcrypt.hash(req.body.pw, 10, function(err, hash) {
-      new_pw = hash;
-    });
-    Users.updateOne({id: req.body.id}, {pw: hash},
-    function (err, res) {
-        if(err) console.log(err);
+      Users.updateOne({id: req.body.id}, {pw: hash},
+      function (err, res) {
+          if(err) console.log(err);
+      });
     });
     return res.status(200).json({message: "success"});
   });
